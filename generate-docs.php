634f5101db89ed2697a8197f186fd79d2de6af33
Davey Shafik <me@daveyshafik.com>
2013-07-02 07:24:29 -0400

    Don't fix top nav as this has been fixed upstream
    This solves the mojibake issue also

--- generate-docs.php
+++ generate-docs.php
@@ -1,22 +1,4 @@
 <?php
-function removeNavMenu($file) {
-    $dom = new DomDocument('5', 'utf-8');
-    $dom->loadHTMLFile($file);
-
-    $xpath = new DomXpath($dom);
-
-    // Remove Nav Menu
-    $result = $xpath->query('//ul[@class="nav"]/li/ul');
-    if ($result->length > 0) {
-        $node = $result->item(0);
-        if ($node->childNodes->length == 0) {
-            $node->parentNode->parentNode->removeChild($node->parentNode);
-        }
-    }
-
-    $dom->saveHTMLFile($file);
-}
-
 $result = 0;
 passthru('cd ' . __DIR__ . ' && phpdoc --force', $result);
 
@@ -25,16 +7,6 @@ if ($result !== 0) {
 }
 
 libxml_use_internal_errors(true);
-
-$files = new RegexIterator(
-    new RecursiveIteratorIterator(new RecursiveDirectoryIterator('./docs')),
-    '/^.+\.html$/i',
-    RecursiveRegexIterator::GET_MATCH
-);
-
-foreach ($files as $file) {
-    removeNavMenu($file[0]);
-}
 
 $dom = new DomDocument();
 $dom->loadHTMLFile("./docs/index.html");
